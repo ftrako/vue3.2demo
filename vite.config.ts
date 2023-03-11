1510ce3b5f31fe5cb2f97c9cd83623a8fe8056e9
chendajian <chen.dajian@aliyun.com>
2023-03-11 16:25:04 +0800

    支持热更新

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 8080
+    port: 8080,
+    open: false, // true表示自动打开浏览器
+    hmr: true // 热更新
   },
   plugins: [vue()],
 })
