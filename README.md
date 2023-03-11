82ded54b327a4a5cc0d4b1fb631b3c8c495c4648
chendajian <chen.dajian@aliyun.com>
2023-03-11 16:24:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Vue 3 + TypeScript + Vite
+
+vite启动速度超快
+
+## 创建工程
+拉取空git工程进入vue3.2demo目录后，执行
+```aidl
+yarn create vite . --template vue-ts
+```
+
+## 运行本地环境
+```aidl
+yarn dev
+```
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import {ref} from "vue";
+import Parent from "./components/Parent.vue";
+
+let msg = ref(0)
+const add = () => {
+  msg.value++
+}
+</script>
+
+<template>
+  {{ msg }}
+  <button @click="add">+</button><br />
+</template>
+
+<style scoped>
+</style>
